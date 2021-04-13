196e234f341411d97bf4684bdae86af9db6543ad
felinae <felinae@ulthar.cat>
2021-04-13 03:43:38 -0700

    renaming

--- CounterStrikeGO/@setup.bat
+++ CounterStrikeGO/@setup.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 
-REM set the game directory
+:: set the game directory
 SET GameRoot=C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\
 
-REM create folders in case they dont exist
+:: create folders in case they dont exist
 MKDIR "%GameRoot%csgo"
 MKDIR "%GameRoot%csgo\cfg"
 
-REM symlink folders
+:: symlink folders
 MKLINK /D "%GameRoot%csgo\cfg\user" "%~dp0%csgo\cfg\user"
 
-REM symlink files
+:: symlink files
 MKLINK "%GameRoot%csgo\cfg\autoexec.cfg" "%~dp0%csgo\cfg\autoexec.cfg"
 
 PAUSE
--- MouseSettings/@setup.bat
+++ MouseSettings/@setup.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-REM set the game directory
+:: set the game directory
 SET RawAccelRoot=C:\Tools\RawAccel\
 SET SensMatchRoot=C:\Tools\SensitivityMatcher\
 
-REM symlink files
+:: symlink files
 MKLINK "%RawAccelRoot%settings.json" "%~dp0%RawAccel\settings.json"
 MKLINK "%SensMatchRoot%CustomYawList.ini" "%~dp0%SensitivityMatcher\CustomYawList.ini"
 
