b088ac1ae2ecce14918c04055e7466beb5c9a482
felinae <felinae@ulthar.cat>
2021-04-19 01:40:15 -0700

    BEGONE MOUSE ACCEL, I NEED U NO MORE

--- MouseSettings/@setup.bat
+++ MouseSettings/@setup.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 
 :: set the game directory
-SET RawAccelRoot=C:\Tools\RawAccel\
 SET SensMatchRoot=C:\Tools\SensitivityMatcher\
 
 :: symlink files
-MKLINK "%RawAccelRoot%settings.json" "%~dp0%RawAccel\settings.json"
 MKLINK "%SensMatchRoot%CustomYawList.ini" "%~dp0%SensitivityMatcher\CustomYawList.ini"
 
 PAUSE
