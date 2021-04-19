a09455ee863a18ad5f11e6a955f3db116a66381e
felinae <felinae@ulthar.cat>
2021-04-19 16:51:38 -0700

    add hypnotize hud submodile and a customization

---
+++ TeamFortress2/patches.bat
@@ -0,0 +1,12 @@
+
+
+:::::::::::::::::::
+:: HYPNOTIZE HUD ::
+:::::::::::::::::::
+
+:: supported customizations
+ROBOCOPY /S /E "tf\custom\hud\customizations\healthbox overrides\resource" "tf\custom\hud\resource"
+ROBOCOPY /S /E "tf\custom\hud\customizations\healthbox overrides\scripts"  "tf\custom\hud\scripts"
+
+
+pause
