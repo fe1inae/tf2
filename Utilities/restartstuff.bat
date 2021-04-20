2d50eac7c2ee9bb18931670eb7bd22a61cc01dae
felinae <felinae@ulthar.cat>
2021-04-19 23:27:05 -0700

    add restart script

---
+++ Utilities/restartstuff.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+:: both sometimes crank cpu usage for no reason, just script to restart
+
+TASKKILL /IM "obs64.exe" /F
+TIMEOUT /NOBREAK /T 2
+START /B /D"C:\Program Files\obs-studio\bin\64bit" "" "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer"
+
+TASKKILL /IM "NVIDIA RTX Voice.exe" /F
+TIMEOUT /NOBREAK /T 2
+START /MIN /B /D"C:\Program Files\NVIDIA Corporation\NVIDIA RTX Voice" "" "C:\Program Files\NVIDIA Corporation\NVIDIA RTX Voice\NVIDIA RTX Voice.exe"
+
+
