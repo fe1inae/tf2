09774723f7c1ec2a86d61d54e355caceb90dbed4
felinae <felinae@ulthar.cat>
2021-04-22 23:42:23 -0700

    add back mouseaccel cause im dummy...

--- MouseSettings/@setup.bat
+++ MouseSettings/@setup.bat
@@ -2,9 +2,11 @@
 
 :: set the game directory
 SET SensMatchRoot=C:\Tools\SensitivityMatcher\
+SET RawAccelRoot=C:\Tools\RawAccel\
 
 :: symlink files
 MKLINK "%SensMatchRoot%CustomYawList.ini" "%~dp0%SensitivityMatcher\CustomYawList.ini"
+MKLINK "%RawAccelRoot%settings.json"      "%~dp0%RawAccel\settings.json"
 
 PAUSE
 
