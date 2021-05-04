8d767adecfcf9bae486f18395f0b4b287351e9a9
felinae <felinae@ulthar.cat>
2021-05-03 23:52:30 -0700

    misc

--- MouseSettings/@setup.bat
+++ MouseSettings/@setup.bat
@@ -4,6 +4,9 @@
 SET SensMatchRoot=C:\Tools\SensitivityMatcher\
 SET RawAccelRoot=C:\Tools\RawAccel\
 
+DEL "%RawAccelRoot%settings.json"
+DEL "%SensMatchRoot%CustomYawList.ini"
+
 :: symlink files
 MKLINK "%SensMatchRoot%CustomYawList.ini" "%~dp0%SensitivityMatcher\CustomYawList.ini"
 MKLINK "%RawAccelRoot%settings.json"      "%~dp0%RawAccel\settings.json"
